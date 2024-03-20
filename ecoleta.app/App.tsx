44306101f5bd861f2f8ad94a85dee482354795b9
Alerrandro Tomé <alerrandrotome025@gmail.com>
2024-03-20 10:48:55 -0300

    Start the app project with home page

---
+++ ecoleta.app/App.tsx
@@ -0,0 +1,42 @@
+import React, { useEffect, useCallback } from 'react';
+import Home from './src/pages/Home';
+import { StatusBar, SafeAreaView } from 'react-native';
+import { Roboto_400Regular, Roboto_500Medium } from '@expo-google-fonts/roboto';
+import { Ubuntu_700Bold, useFonts } from '@expo-google-fonts/ubuntu';
+import * as SplashScreen from 'expo-splash-screen';
+
+export default function App() {
+  const [fontsLoaded] = useFonts({
+    Roboto_400Regular,
+    Roboto_500Medium,
+    Ubuntu_700Bold
+  });
+
+  useEffect(() => {
+    async function prepare() {
+      try {
+        await SplashScreen.preventAutoHideAsync();
+      } catch (e) {
+        console.warn(e);
+      }
+    }
+    prepare();
+  }, []);
+
+  const onLayoutRootView = useCallback(async () => {
+    if (fontsLoaded) {
+      await SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded]);
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
+  return (
+    <SafeAreaView style={{flex: 1}} onLayout={onLayoutRootView}>
+      <StatusBar barStyle="dark-content" backgroundColor="transparent" translucent />
+      <Home />
+    </SafeAreaView>
+  );
+}
